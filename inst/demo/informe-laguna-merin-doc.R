9389f0eedb0a10ffefb412f4dad8e17e2bebae9f
Juan Manuel Barreneche <jumanbar@gmail.com>
2021-03-19 14:30:15 -0300

    Arreglito en demos
    Necesario porque a veces rstudioapi::documentNew no funciona (si la versión de RStudio es más antigua que la primer versión de la función, básicamente)

--- inst/demo/informe-laguna-merin-doc.R
+++ inst/demo/informe-laguna-merin-doc.R
@@ -1,8 +1,9 @@
 extension <- 'doc'
 demoname <- paste0('informe-laguna-merin-', extension, '.Rmd')
 demofile <- system.file("examples", demoname, package = "manoSIAR")
-
-if (grepl('rstudio', .Platform$GUI, ignore.case = TRUE)) {
+rs <- grepl('rstudio', .Platform$GUI, ignore.case = TRUE) &
+  RStudio.Version()$version >= '1.2.640'
+if (rs) {
   contenidos <- readLines(demofile)
   rstudioapi::documentNew(paste(contenidos, collapse = '\n'), 'rmarkdown')
 } else {
@@ -14,6 +15,8 @@ if (grepl('rstudio', .Platform$GUI, ignore.case = TRUE)) {
     i <- i + 1
   }
   file.copy(demofile, demoname)
-  message('Se cre\u00f3 el archivo ', demoname, ' en la carpeta de trabajo')
+  message('ATENCI\u00d3N: Se cre\u00f3 el archivo "',
+          demoname,
+          '" en la carpeta de trabajo')
   file.edit(demoname)
 }
